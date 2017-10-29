859379e650aa4287eb37ed8c2fac7162a388f8bd
nave7693 <nave7693@nave7693.com>
2017-10-29 00:25:51 -0700

    All the Blaze is gone! Yay!

--- imports/ui/components/hello/hello.jsx
+++ imports/ui/components/hello/hello.jsx
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
 
 class Hello extends Component {
+  constructor(props) {
+    super(props);
+    this.buttonClick = this.buttonClick.bind(this);
+    this.state = {
+      counter: 0,
+    };
+  }
+
+  buttonClick() {
+    this.setState({
+      counter: this.state.counter + 1,
+    });
+  }
+
   render() {
     return (
-      <div>HELLO</div>
+      <div>
+        <button onClick={this.buttonClick}>Click Me</button>
+        <p>You've pressed the button {this.state.counter} times.</p>
+      </div>
     );
   }
 }
--- imports/ui/components/info/info.jsx
+++ imports/ui/components/info/info.jsx
@@ -1,11 +1,74 @@
 import React, { Component } from 'react';
+import { PropTypes } from 'prop-types';
+import { withTracker } from 'meteor/react-meteor-data';
+import { Links } from '/imports/api/links/links.js';
+import { Meteor } from 'meteor/meteor';
 
 class Info extends Component {
+  constructor(props) {
+    super(props);
+    this.addLink = this.addLink.bind(this);
+  }
+
+  addLink(event) {
+    event.preventDefault();
+
+    const target = event.target;
+    const title = target.title;
+    const url = target.url;
+
+    Meteor.call('links.insert', title.value, url.value, (error) => {
+      if (error) {
+        alert(error.error);
+      } else {
+        title.value = '';
+        url.value = '';
+      }
+    });    
+  }
+
+  renderLinks(links) {
+    return links.map((link) => {
+      return (
+        <li><a href={ link.url } target="_blank">{ link.title }</a></li>
+      );
+    });
+  }
+
   render() {
+    if (this.props.loading) {
+      return (
+        <div>Loading...</div>
+      );
+    }
     return (
-      <div>INFO</div>
+      <div>
+        <h2>Learn Meteor!</h2>
+        <ul>
+          <li>
+            <form className="info-link-add" onSubmit={this.addLink}>
+              <input type="text" name="title" placeholder="Title" required />
+              <input type="url" name="url" placeholder="Url" required />
+              <input type="submit" name="submit" value="Add new link" />
+            </form>
+          </li>
+          { this.renderLinks(this.props.links) }
+        </ul>
+      </div>
     );
   }
 }
 
-export default Info;
+Info.propTypes = {
+  loading: PropTypes.bool.isRequired,
+  links: PropTypes.array.isRequired,
+};
+
+export default withTracker(() => {
+  const handle = Meteor.subscribe('links.all');
+
+  return {
+    loading: !handle.ready(),
+    links: Links.find({}).fetch()
+  }
+})(Info);
